87538d62dc9dac93f542be7ecbc5ff649c221988
athul <athul8720@gmail.com>
2019-11-15 22:55:52 +0530

    API Routes

--- main.go
+++ main.go
@@ -18,5 +18,11 @@ func main() {
 			"message": "Wubba Lubba Dub Dub",
 		})
 	})
+	api := r.Group("/api")
+	api.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Init API",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
